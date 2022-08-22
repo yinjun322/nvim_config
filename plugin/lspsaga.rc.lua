1c55f3bf59c37ad7091a00d41abe42d20a58eb68
MarkYin <yinjun322@gmail.com>
2022-08-22 19:04:39 +0800

    update

--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -8,8 +8,8 @@ saga.init_lsp_saga {
 }
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', 'n', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
-vim.keymap.set('n', '<S-n>', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
+vim.keymap.set('n', '<C-n>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+vim.keymap.set('n', '<C-p>', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
