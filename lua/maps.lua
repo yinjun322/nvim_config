f021547bd9f49cbc0e08e08376b8fe10387c1bde
MarkYin <yinjun322@gmail.com>
2022-08-28 17:30:32 +0800

    update

--- lua/maps.lua
+++ lua/maps.lua
@@ -34,7 +34,8 @@ keymap.set('n', '+', '<C-a>')
 keymap.set('n', '-', '<C-x>')
 
 -- Delete a word backwards
-keymap.set('n', 'dw', 'vb"_d')
+-- 删除前面一个词
+-- keymap.set('n', 'dw', 'vb"_d')
 
 -- Select all
 keymap.set('n', '<C-a>', 'gg<S-v>G')
